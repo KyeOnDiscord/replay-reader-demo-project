232ce41d7b657f66b18229962c3d0cfcb22a8231
Kye <promasterboy@outlook.com>
2023-10-02 17:31:47 +1100

    init

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const replayReader = require('fortnite-replay-parser');
-const handleEventEmitter = require('./exports/handleEventEmitter');
-const NetFieldExports = require('./NetFieldExports');
-const customClasses = require('./Classes');
-const fs = require('fs');
+const replayReader = require("fortnite-replay-parser");
+const handleEventEmitter = require("./exports/handleEventEmitter");
+const NetFieldExports = require("./NetFieldExports");
+const customClasses = require("./Classes");
+const fs = require("fs");
 
 (async () => {
-  const replayBinary = fs.readFileSync('replays/server-17.21.replay');
+  const replayBinary = fs.readFileSync(
+    "replays/UnsavedReplay-2023.10.02-17.11.40.replay"
+  );
 
   console.time();
   const replay = await replayReader(replayBinary, {
@@ -13,8 +15,9 @@ const fs = require('fs');
     customNetFieldExports: NetFieldExports,
     onlyUseCustomNetFieldExports: true,
     customClasses,
+    debug: true,
   });
   console.timeEnd();
 
-  fs.writeFileSync('result.json', JSON.stringify(replay, null, 2));
+  fs.writeFileSync("result.json", JSON.stringify(replay, null, 2));
 })();
